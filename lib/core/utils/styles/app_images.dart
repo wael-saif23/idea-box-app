3e49f43e7dcb24cf22643765e3d2792ae9d7de99
wael-saif23 <ws.mywork@gmail.com>
2024-09-03 03:48:22 +0300

    generate assets images

---
+++ lib/core/utils/styles/app_images.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  /// Assets for imagesMainAppIcon
+  /// assets/images/main_app_icon.png
+  static const String imagesMainAppIcon = "assets/images/main_app_icon.png";
+
+  /// Assets for imagesNotFoundImage
+  /// assets/images/not_found_image.svg
+  static const String imagesNotFoundImage = "assets/images/not_found_image.svg";
+
+  /// Assets for imagesNoteImage
+  /// assets/images/note_image.svg
+  static const String imagesNoteImage = "assets/images/note_image.svg";
+}
+
